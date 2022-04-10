17cc9b991790863463bb74d177b414a3fbc08b93
Nishant Raj <nrnishant97@gmail.com>
2022-04-10 14:46:27 +0530

    Day 2change

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class  HomePage extends StatelessWidget {
+  final int days=30;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("catlogue APP"),
+      ),
+        body: Center(
+          child: Container(
+            child: Text('Welcome to day $days of my new flutter nishant'),
+          ),
+        ),
+        drawer: Drawer(),
+      );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-
+import 'home_page.dart';
 void main() {
   runApp(MyApp());
 }
@@ -9,14 +9,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int days = 30;
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome to my new flutter nishant'),
-          ),
-        ),
-      ),
+      home: HomePage();
     );
   }
 }
